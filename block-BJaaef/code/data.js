ec724835ed30b3a73bdb8b1262248d9c6e5c3b19
Aman Thakur <uttamthakur2999@gmail.com>
2021-06-04 19:11:39 +0530

    GOT People

--- block-BJaaef/code/data.js
+++ block-BJaaef/code/data.js
@@ -326,3 +326,26 @@ let got = {
     },
   ],
 };
+
+
+
+
+let ul = document.querySelector("ul");
+    got.houses.forEach(house => {
+    house.people.forEach(peoples => { 
+   let li = document.createElement("li")
+   li.classList.add("card","flex-30")
+   let img = document.createElement("img")
+   img.src =peoples.image
+   let span = document.createElement("span")
+   span.innerText =peoples.name
+   let h2  =document.createElement("h2")
+   h2.innerText =peoples.description
+   let button =document.createElement("button")
+   button.classList.add("btn")
+   button.innerText ="Learn More"
+   li.append(img,span, h2, button)
+   ul.append(li)
+  console.log(ul);
+    })
+      });
